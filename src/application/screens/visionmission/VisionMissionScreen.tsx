758eeb4a6cd6b9c402e7b87a2dcfeaef47e4300a
Lephta <39690511+lephta@users.noreply.github.com>
2025-10-24 01:32:58 +0200

    Contact page

--- src/application/screens/visionmission/VisionMissionScreen.tsx
+++ src/application/screens/visionmission/VisionMissionScreen.tsx
@@ -5,48 +5,86 @@ import VisionImage from "../../../assets/images/vision.png";
 import { GetIcon } from "../../../infrastructure/shared/icons/GetIcon";
 
 export const VisionMissionScreen: React.FC<{}> = (): JSX.Element => {
-    return <div className="screen-container vision-mission">
-        <div className="screen-content">
-            <h1>Vision Mission</h1>
-            <RedBoarder />
+  return (
+    <div className="screen-container vision-mission">
+      <div className="screen-content">
+        <h1>Vision Mission</h1>
+        <RedBoarder />
 
-            <div className="upper-content flex">
-                <div className="left-area">
-                    <div className="vision-area">
-                        <h4 className="font-bold">Vision</h4>
-                        <p>ActionAid South Africa (AASA) has a clear <b>vision</b> of realising <b>social justice</b> through supporting the
-                            <b>actions</b> and <b>power of people</b> whose lives are most affected by inequality, injustice and oppression.
-                            And so, we commit to supporting social movements of the poor, excluded and marginalised,
-                            prioritising our support for womxn-led feminist movements.</p>
+        <div className="upper-content flex">
+          <div className="left-area">
+            <div className="vision-area">
+              <h4 className="font-bold">Vision</h4>
+              <p>
+                ActionAid South Africa (AASA) has a clear <b>vision</b> of
+                realising <b>social justice</b> through supporting the
+                <b>actions</b> and <b>power of people</b> whose lives are most
+                affected by inequality, injustice and oppression. And so, we
+                commit to supporting social movements of the poor, excluded and
+                marginalised, prioritising our support for womxn-led feminist
+                movements.
+              </p>
 
-                        <p>
-                            In a post-apartheid South Africa, we have seen some successes in realising individual rights,
-                            but collective <b>rights’ realisation</b> of people living in poverty and marginalisation, has been limited.
-                            To AASA, <b>structural change</b> is required to realise <b>social justice</b>.
-                        </p>
-                    </div>
-                    <div className="mission-area">
-                        <h4 className="font-bold">Mission</h4>
-                        <p>To Work With Poor And Excluded People To Eradicate Poverty And Injustice. To eradicate poverty and injustice by working with people living in poverty and exclusion, civil society organisations, activists and movements wherever they are.</p>
-                    </div>
-                </div>
-                <div className="right-area">
-                    <img src={VisionImage} alt="vision" />
-                </div>
+              <p>
+                In a post-apartheid South Africa, we have seen some successes in
+                realising individual rights, but collective{" "}
+                <b>rights’ realisation</b> of people living in poverty and
+                marginalisation, has been limited. To AASA,{" "}
+                <b>structural change</b> is required to realise{" "}
+                <b>social justice</b>.
+              </p>
             </div>
-            <div className="lower-content">
-                <h4 className="font-bold">Organisational Values</h4>
-                <ul>
-                    <li><GetIcon iconName="bi bi-circle-fill" />&nbsp;&nbsp;<span className="font-bold text-red-800">Feminist 2:</span> AASA aims to create a society in which individuals’ genders don’t restrict them from an equitable access to rights and resources.</li>
-                    <li><GetIcon iconName="bi bi-circle-fill" />&nbsp;&nbsp;<span className="font-bold text-red-800">Mutual Respect:</span> requiring us to recognise the innate worth of all people and the value of diversity.</li>
-                    <li><GetIcon iconName="bi bi-circle-fill" />&nbsp;&nbsp;<span className="font-bold text-red-800">Equity and Justice:</span> ensuring equal opportunity to everyone, irrespective of race, age, gender, sexual orientation, HIV status, colour, class, ethnicity, disability, location and religion.</li>
-                    <li><GetIcon iconName="bi bi-circle-fill" />&nbsp;&nbsp;<span className="font-bold text-red-800">Integrity:</span> requiring us to be honest, transparent and accountable at all levels for the effectiveness of our actions and open in our judgements and communication with others.</li>
-                    <li><GetIcon iconName="bi bi-circle-fill" />&nbsp;&nbsp;<span className="font-bold text-red-800">Solidarity:</span> with people living in poverty and exclusion will be the only bias in our commitment to the fight against poverty.</li>
-                    <li><GetIcon iconName="bi bi-circle-fill" />&nbsp;&nbsp;<span className="font-bold text-red-800">Courage of Conviction:</span> requiring us to be creative and radical, bold and innovative – without fear of failure – in pursuit of making the greatest possible impact on the causes of poverty and injustice.</li>
-                    <li><GetIcon iconName="bi bi-circle-fill" />&nbsp;&nbsp;<span className="font-bold text-red-800">Independence:</span> from any religious and party-political affiliation.</li>
-                    <li><GetIcon iconName="bi bi-circle-fill" />&nbsp;&nbsp;<span className="font-bold text-red-800">Humility:</span> in our presentation and behaviour, recognising that we are part of a wider alliance against poverty and inequality.</li>
-                </ul>
+            <div className="mission-area">
+              <h4 className="font-bold">Mission</h4>
+              <p>
+                To Work With Poor And Excluded People To Eradicate Poverty And
+                Injustice. To eradicate poverty and injustice by working with
+                people living in poverty and exclusion, civil society
+                organisations, activists and movements wherever they are.
+              </p>
             </div>
+          </div>
+          <div className="right-area">
+            <img src={VisionImage} alt="vision" />
+          </div>
         </div>
+        <div className="et_pb_row et_pb_row_1">
+          <div className="et_pb_column et_pb_column_1_2 et_pb_column_1  et_pb_css_mix_blend_mode_passthrough">
+            <div className="et_pb_module et_pb_text et_pb_text_1  et_pb_text_align_left et_pb_bg_layout_light">
+              <div className="et_pb_text_inner">
+                <h1>Current Interventions</h1>
+                <div className="separator"></div>
+                <p>
+                  Our corporate alliance is active and growing. We are currently
+                  partnering with socially responsible corporates on diverse
+                  causes like womxn, Child education with special emphasis on
+                  girl child education, sustainable agriculture, climate change,
+                  bonded labours , skill building for informal sector workers
+                  and their livelihoods, urban poverty.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="et_pb_column et_pb_column_1_2 et_pb_column_2  et_pb_css_mix_blend_mode_passthrough et-last-child">
+            <div className="et_pb_module et_pb_text et_pb_text_2  et_pb_text_align_left et_pb_bg_layout_light">
+              <div className="et_pb_text_inner">
+                <h1>Transparency &amp; Accountability</h1>
+                <div className="separator"></div>
+                <p>
+                  We strive to develop our programmes with the involvement of
+                  the communities we claim to work with. This makes transparency
+                  a dual responsibility for us- we are as accountable to you,
+                  our donor and we are equally responsible to the communities we
+                  work with. This will be visible in the way we report back-
+                  while we send updates, event reports, regular reports to our
+                  funders, we also ensure that the money we spend on a community
+                  is known to them.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-}
+  );
+};
--- src/infrastructure/navigation/AppNavigation.tsx
+++ src/infrastructure/navigation/AppNavigation.tsx
@@ -12,16 +12,31 @@ import { PressRelease } from "../../application/screens/pressrelease/PressReleas
 import { WhereWeWorkScreen } from "../../application/screens/wherewework/WhereWeWorkScreen";
 import { PressReleaseView } from "../../application/screens/pressreleaseview/PressReleaseView";
 import { PublicationsScreen } from "../../application/screens/publications/PublicationsScreen";
+<<<<<<< Updated upstream
 import { ProgrammeApproach } from "../../application/screens/programapproach/ProgrammeApproach";
 import { VisionMissionScreen } from "../../application/screens/visionmission/VisionMissionScreen";
 import { PublicationViewScreen } from "../../application/screens/publicationsview/PublicationsViewScreen";
 import { StrategicPrioritiesScreen } from "../../application/screens/strategicpriorities/StrategicPrioritiesScreen";
+=======
+import { PublicationViewScreen } from "../../application/screens/publicationsview/PublicationsViewScreen";
+import { VisionMissionScreen } from "../../application/screens/visionmission/VisionMissionScreen";
+import { CooperateEngagementScreen } from "../../application/screens/cooperateengagement/CooperateEngagementScreen";
+import { WhereWeWorkScreen } from "../../application/screens/wherewework/WhereWeWorkScreen";
+import { StrategicPrioritiesScreen } from "../../application/screens/strategicpriorities/StrategicPrioritiesScreen";
+import { ProgrammeApproach } from "../../application/screens/programapproach/ProgrammeApproach";
+import { ContactUsScreen } from "../../application/screens/contactus/ContactUsScreen";
+import { OurTeamsScreen } from "../../application/screens/ourteams/OurTeamsScreen";
+
+
+>>>>>>> Stashed changes
 
 export const AppNavigation: React.FC<{}> = (): JSX.Element => (
   <BrowserRouter>
     <Routes>
       <Route element={<AppLayout />}>
         <Route element={<LandingScreen />} path="/" />
+        <Route element={<ContactUsScreen />} path="/contact-us" />
+        <Route element={<OurTeamsScreen />} path="/our-teams" />
         <Route element={<BlogScreen />} path="/blog" />
         <Route element={<Stories />} path="/stories" />
         <Route element={<DonateScreen />} path="/donations" />
@@ -32,10 +47,20 @@ export const AppNavigation: React.FC<{}> = (): JSX.Element => (
         <Route element={<WhereWeWorkScreen />} path="/where-we-work" />
         <Route element={<PublicationsScreen />} path="/publications" />
         <Route element={<VisionMissionScreen />} path="/vision-mission" />
+<<<<<<< Updated upstream
         <Route element={<ProgrammeApproach />} path="/programme-approach" />
         <Route element={<PublicationViewScreen />} path="/publication/:name" />
         <Route element={<PressReleaseView />} path="/press-release-view/:name" />
         <Route element={<StrategicPrioritiesScreen />} path="/strategic-priorities" />
+=======
+        <Route element={<CooperateEngagementScreen />} path="/Cooperate-Engagement" />
+        <Route element={<PublicationViewScreen />} path="/publication/:name" />
+        <Route element={<PressReleaseView />} path="/press-release-view/:name" />
+        <Route element={<StrategicPrioritiesScreen />} path="/strategic-priorities" />
+        <Route element={<ProgrammeApproach />} path="/programme-approach" />
+       
+
+>>>>>>> Stashed changes
       </Route>
     </Routes>
   </BrowserRouter>
